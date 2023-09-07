bad17d793002243994aa011c27f8517d25822204
Seyi <kuforiji98@gmail.com>
2023-09-07 15:12:21 +0100

    updating app.py and readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# zuri_api
+
+# Zuri api project
+
+This is a simple backend api project which processes a get request of two query string slack_name and track to displays the rest of the object.
+
+## Authors
+
+- [@seyi007](https://www.github.com/seyi007)
+
+## Tech stack
+
+- Python
+- flask
+
+## Description
+
+To use api do:
+
+- use `(http://127.0.0.1:5000/api?slack_name=name&track=track)`
+- assign your name into `slack_name` variable and your track into `track` variable
--- app.py
+++ app.py
@@ -1,21 +1,29 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify, request
+from datetime import datetime
 
 app = Flask(__name__)
 
-task = {
-  "slack_name": "example_name",
-  "current_day": "Monday",
-  "utc_time": "2023-08-21T15:04:05Z",
-  "track": "backend",
-  "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-  "github_repo_url": "https://github.com/username/repo",
-  "status_code": 200
-}
+@app.route('/api', methods=['GET'])
+def get_item():
+  """Retrieves all data of the item with the slack_name and track param"""
+  slack_name = request.args.get('slack_name')
+  current_day = datetime.now().strftime('%A')
+  utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+  track = request.args.get('track')
+  github_file_url = 'https://github.com/Seyi007/zuri_api/blob/main/app.py'
+  github_repo_url = "https://github.com/Seyi007/zuri_api"
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+  res = {
+     "slack_name": slack_name,
+     "current_day": current_day,
+     "utc_time": utc_time,
+     "track": track,
+     "github_file_url": github_file_url,
+     "github_repo_url": github_repo_url,
+     "status_code": 200,
+  }
+  return jsonify({'res': res})
 
 if __name__ == '__main__':
     app.run(debug=True)
